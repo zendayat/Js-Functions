25a70093dd4790c16b3633c9c547be148e732435
zendayat <nmapzendaya@gmail.com>
2024-05-14 01:43:58 +0400

    example 1

--- map.js
+++ map.js
@@ -1,1 +1,11 @@
 // THIS IS A PRACTICE ON HOW THE map() function works
+
+const Numbers = [1,2,3,4,5];
+const squares = Numbers.map(square);
+
+console.log(squares);
+
+
+function square(element) {
+    return Math.pow(element, 2);
+}
